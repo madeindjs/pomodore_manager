a94e3a3b70c3cacf5e8587e7e101a1f5dc299166
Rousseau Alexandre <a.rousseau@carrier-ref.fr>
2016-03-31 11:02:42 +0200

    setup controller and view for Shops and update the navbar

---
+++ Reports_Manager/Controllers/ShopsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reports_Manager.Controllers
+{
+    public class ShopsController : Controller
+    {
+        // GET: Shops
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Shops/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        
+    }
+}
