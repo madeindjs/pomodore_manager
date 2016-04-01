9fdc61b001bafbc1d14e9878375250561e5d25a3
Rousseau Alexandre <a.rousseau@carrier-ref.fr>
2016-04-01 13:14:29 +0200

    Setup Shops#details & update error page

--- Reports_Manager/Controllers/ShopsController.cs
+++ Reports_Manager/Controllers/ShopsController.cs
@@ -15,8 +15,8 @@ namespace Reports_Manager.Controllers
         // GET: Shops/index
         public ActionResult Index()
         {
-            var database_Shops = database.Shops;
-            
+            System.Data.Entity.DbSet<Shop> database_Shops = database.Shops;
+
             if (database_Shops != null)//I check if there are data into database
             {
                 //I get shoplist from database & I group them by OTP
@@ -31,11 +31,31 @@ namespace Reports_Manager.Controllers
         }
 
         // GET: Shops/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(string id)
         {
-            return View();
-        }
+            if(id != null)
+            {
+                System.Data.Entity.DbSet<Shop> database_Shops = database.Shops;
+                List<Reports_Manager.Models.Shop> shops_list = database_Shops.ToList();
 
-        
+                ViewBag.shop = shops_list.Find(shop => shop.Otp == id);
+
+                if (ViewBag.shop != null)
+                {
+                    return View();
+                }
+                else
+                {
+                    ViewBag.error = "Cet OTP n'est pas dans la base de données...";
+                    return View("./Error");
+                }
+            }
+            else
+            {
+                ViewBag.error = "Erreur lors de la récupration du paramètre GET";
+                return View("./Error");
+            }
+
+        }
     }
 }
