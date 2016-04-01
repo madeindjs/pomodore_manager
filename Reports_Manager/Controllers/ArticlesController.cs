9e6e2419a6f6f5e13817051ae46a1ec5e5082aed
Rousseau Alexandre <a.rousseau@carrier-ref.fr>
2016-04-01 14:21:57 +0200

    Add ArticlesController, add Articles#Index & Articles#details and update navbar

---
+++ Reports_Manager/Controllers/ArticlesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reports_Manager.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        // GET: Articles/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Articles/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+    }
+}
