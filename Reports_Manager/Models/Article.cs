322040ecb7823d3db943d2680d3965b5a248be26
Rousseau Alexandre <a.rousseau@carrier-ref.fr>
2016-04-06 10:53:21 +0200

    create Article model, and update Articles#index

---
+++ Reports_Manager/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Reports_Manager.Models
+{
+    public class Article
+    {
+        
+        public string Id { get; set; }
+        public string Designation { get; set; }
+        public string Gamme { get; set; }
+        public string Longueur { get; set;  }
+        public string Usine { get; set; }
+        [Key]
+        public int Sql_id { get; set; }
+
+    }
+}
