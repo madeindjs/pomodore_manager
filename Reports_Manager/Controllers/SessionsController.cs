16307b36225e0d4785f34090f12e94f8ca0e4401
Rousseau Alexandre <a.rousseau@carrier-ref.fr>
2016-04-12 15:46:16 +0200

    improve design

--- Reports_Manager/Controllers/SessionsController.cs
+++ Reports_Manager/Controllers/SessionsController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Reports_Manager.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,24 +11,42 @@ namespace Reports_Manager.Controllers
     public class SessionsController : Controller
     {
 
+        private CarrierDataEntities database = new CarrierDataEntities();
 
         // GET: Session/Create
         public ActionResult Create()
         {
-            return View();
-        }
-
-        // POST: Session/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
+            if (Request.RequestType == "POST")
             {
-                // TODO: Add insert logic here
+                NameValueCollection post_data = Request.Form;
+                System.Data.Entity.DbSet<User> database_Users = database.Users;
+                string email_input = post_data["email"];
+                User user = new User();
 
-                return RedirectToAction("Index");
+                try
+                {
+                    user = database_Users.First(users => users.Email == email_input);
+                }
+                catch(Exception e)
+                {
+                    ViewBag.error = e.Message;
+                    //ViewBag.error = "Cet email n'est pas présent dans la database";
+                    return View("./Error");
+                }
+
+                if (DecryptPassword(user.Password) == post_data["password"])
+                {
+                    return RedirectToAction("Index", "Shops", new { area = "" });
+                }
+                else
+                {
+                    ViewBag.error = "Le mot de passe ne correspond pas";
+                    return View("./Error");
+                }
+
+
             }
-            catch
+            else
             {
                 return View();
             }
@@ -34,10 +54,19 @@ namespace Reports_Manager.Controllers
 
 
 
+
         // GET: Session/Delete/5
         public ActionResult Delete()
         {
             return Redirect("/" );
+        }
+
+        private string DecryptPassword(string encryptedPassword)
+        {
+            //Decrypter le mot de passe    
+            byte[] passByteData = Convert.FromBase64String(encryptedPassword);
+            string originalPassword = System.Text.Encoding.Unicode.GetString(passByteData);
+            return originalPassword;
         }
 
     }
