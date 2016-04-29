04378eccb71235b4f630622a2d58e3e6b3fced88
Rousseau <RousseauA@eu.carrier.UTC.com>
2016-04-29 15:24:18 +0200

    install global variable with technical price per hour

--- Reports_Manager/Models/Shop.cs
+++ Reports_Manager/Models/Shop.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Reports_Manager.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -39,6 +41,32 @@ namespace Reports_Manager.Models
             CarrierDataEntities database = new CarrierDataEntities();
             this.Reports = database.Reports.Where( report => report.Shop_otp == Otp).ToList() ;
         }
+
+        public TimeSpan? time_spend()
+        {
+            TimeSpan? time_spend = TimeSpan.Parse("00:00:00");
+            foreach( Report report in this.Reports)
+            {
+                if (report.T_spend != null)
+                {
+                    time_spend = time_spend + report.T_spend;
+                }
+                if (report.T_travel != null)
+                {
+                    time_spend = time_spend + report.T_travel;
+                }
+            }
+
+            return time_spend ;
+        }
+
+        public string expense()
+        {
+            TimeSpan time_spend = (TimeSpan)this.time_spend();
+            double total_cost = time_spend.TotalHours * Settings.Default.technic_cost_per_hour;
+            return total_cost.ToString("0.00 €");
+        }
+
 
         public string print_adress()
         {
