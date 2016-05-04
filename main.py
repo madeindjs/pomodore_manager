ac4b093fcba7947a946e5cc9ef02f8d7c20c193a
Rousseau <RousseauA@eu.carrier.UTC.com>
2016-05-04 13:16:28 +0200

    initial set of sqlite

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sqlite3
 
-print("hello world")
+try:
+	conn = sqlite3.connect('databases.sqlite')
+except:
+	print("error in database creation")
+
+
+
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS users( id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,name TEXT,age INTERGER ) ")
+
+data = {"name" : "olivier", "age" : 30}
+cursor.execute("INSERT INTO users(name, age) VALUES(:name, :age)", data)
+
+cursor.execute("""SELECT * FROM users""")
+
+for row in cursor:
+	print( 'id:{0} | nom:{1} | age:{2}'.format(row[0], row[1], row[2]))
+
+conn.commit()
+conn.close()
