35d1176e28aa434126cd2aeb18013c90b71c0741
Rousseau Alexandre <madeindjs@gmail.com>
2016-05-06 09:49:28 +0200

    cleaner code again with heritage

--- classes/category.py
+++ classes/category.py
@@ -2,25 +2,14 @@ from classes.database import Database
 
 class Category(Database):
 
+	DATABASE_NAME = 'categories'
+
 	def __init__(self):
 		Database.__init__(self)
 		self.cursor.execute(
 			"CREATE TABLE IF NOT EXISTS categories( " + 
 				"id INTEGER PRIMARY KEY , "+
 				"name TEXT NOT NULL UNIQUE) ")
-
-
-	def find_by_id(self,  id ):
-		data = { "id" : id }
-		self.cursor.execute( "SELECT * FROM categories WHERE id = :id LIMIT 1" , data )
-		return Database.is_data_exists( self.cursor )
-
-
-
-	def find_by_name(self,  name ):
-		data = { "name" : name }
-		self.cursor.execute( "SELECT * FROM categories WHERE name = :name LIMIT 1" , data )
-		return Database.is_data_exists( self.cursor )
 
 
 	def add(self ,  name ):
@@ -31,14 +20,3 @@ class Category(Database):
 			self.connection.commit()
 		else:
 			print('This category already exists')
-
-
-	def list(self):
-		print('----------')
-		print('Categories')
-		print('----------')
-		self.cursor.execute( "SELECT * FROM categories" )
-		for row in self.cursor:
-			print( row )
-		print('----------')
-		
--- classes/database.py
+++ classes/database.py
@@ -2,6 +2,8 @@ import sqlite3
 
 class Database:
 
+	DATABASE_NAME = None
+
 	def __init__(self):
 		try:
 			self.connection = sqlite3.connect('databases.sqlite')
@@ -18,10 +20,36 @@ class Database:
 		self.cursor = self.connection.cursor()
 
 
+
+	def find_by_(self, database, column, column_data ):
+		data = { column : column_data }
+		sql_command = "SELECT * FROM {} WHERE {} = :{} LIMIT 1".format( database , column , column )
+		self.cursor.execute( sql_command , data )
+		return self.is_data_exists( self.cursor )
+
+
+	def find_by_id(self,  id_to_find ):
+		return Database.find_by_(self, self.DATABASE_NAME, 'id', id_to_find )
+
+
+	def find_by_name(self,  name_to_find ):
+		return Database.find_by_( self, self.DATABASE_NAME,  'name', name_to_find )
+
+
 	# to check if cursor have at less one row
-	def is_data_exists(cursor):
+	def is_data_exists(self , cursor):
 		result = cursor.fetchone()
 		if result is None:
 			return False
 		else:
 			return True
+
+	def list(self):
+		print('----------')
+		print(self.DATABASE_NAME)
+		print('----------')
+		self.cursor.execute( "SELECT * FROM {}".format( self.DATABASE_NAME ) )
+		for row in self.cursor:
+			print( row )
+		print('----------')
+
