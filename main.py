2f2308aa33ede4274d2fe199841c27dcf1dba723
madeindjs <madeindjs@gmail.com>
2016-05-13 14:00:06 +0200

    add argument parser

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from classes.task import Task
 from classes.interface import Interface
+import argparse
 
 
 if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-v", "--verbose", action="store_true", help="Show SQL calls")
+	args = parser.parse_args()
+
 	interface = Interface()
