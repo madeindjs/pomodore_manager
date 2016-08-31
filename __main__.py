400d6a298e921fb3f966777d0a9c4ec519a27b92
madeindjs <madeindjs@gmail.com>
2016-08-31 16:22:09 +0200

    do some test

--- __main__.py
+++ __main__.py
@@ -4,11 +4,17 @@ from bottle import route, run, template
 
 def main():
 
+	@route('/home')
+	def home():
+		return template('<h1>{{content}}</h1>', content='Reload works!')
+
+		
+
 	@route('/hello/<name>')
 	def index(name):
 		return template('<b>Hello {{name}}</b>!', name=name)
 
-	run(host='localhost', port=12345)
+	run(host='localhost', port=12345, reloader=True)
 
 
 if __name__ == '__main__':
