0da0a9bcdbc89e8164f9ae4de5be38624024ecc1
madeindjs <madeindjs@gmail.com>
2016-10-06 13:16:05 +0200

    create `Task` & `Category` model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Task(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=250)
+
+
+
+    def __str__(self):
+        return self.name
