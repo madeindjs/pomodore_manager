ee027a45404347479cc9d3eeabb08843f82a27a0
madeindjs <madeindjs@gmail.com>
2016-10-06 14:06:59 +0200

    delete category and apply task's foreign key on itself

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Category, Task
+from .models import Task
 
-admin.site.register(Category)
 admin.site.register(Task)
--- tasks/models.py
+++ tasks/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-
-
-
-    def __str__(self):
-        return self.name
-
-
-
 
 class Task(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    """a task is something to do. Task can also have a parent task in this way
+    it become a sub-task"""
+    parent = models.ForeignKey("self", null=True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=250)
 
