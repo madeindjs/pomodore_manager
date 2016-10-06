f73d022cb787330953321189497a9b3246bb92c7
madeindjs <madeindjs@gmail.com>
2016-10-06 13:40:01 +0200

    build views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-
 from . import views
 
+app_name = 'tasks'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # ex: /tasks/5/
+    url(r'^(?P<task_id>[0-9]+)/$', views.detail, name='detail'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Task
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+    latest_tasks = Task.objects.order_by('id')[:5]
+    template = loader.get_template('tasks/index.html')
+    context = {'latest_tasks': latest_tasks, }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    template = loader.get_template('tasks/details.html')
+    context = {'task': task, }
+    return HttpResponse(template.render(context, request))
