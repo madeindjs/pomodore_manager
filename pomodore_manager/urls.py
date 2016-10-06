69d82334dff81fba15e5d72294e298268f805bbf
madeindjs <madeindjs@gmail.com>
2016-10-06 12:35:00 +0200

    rename categories app as tasks

--- pomodore_manager/urls.py
+++ pomodore_manager/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^categories/', include('categories.urls')),
+	url(r'^tasks/', include('tasks.urls')),
     url(r'^admin/', admin.site.urls),
 ]
