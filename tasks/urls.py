fedcf7336a2e762f982fcc4e7c80f7c21441aa93
madeindjs <madeindjs@gmail.com>
2016-10-06 16:00:54 +0200

    create form

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'tasks'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.new, name='new'),
     # ex: /tasks/5/
     url(r'^(?P<task_id>[0-9]+)/$', views.detail, name='detail'),
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.urls import reverse
 from .models import Task
 
 
@@ -10,8 +11,33 @@ def index(request):
     context = {'latest_tasks': latest_tasks, }
     return HttpResponse(template.render(context, request))
 
+
+
 def detail(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     template = loader.get_template('tasks/details.html')
     context = {'task': task, }
     return HttpResponse(template.render(context, request))
+
+
+
+def new (request):
+    if request.method == 'GET':
+        template = loader.get_template('tasks/new.html')
+        context = {'tasks': Task.objects.all(), }
+        return HttpResponse(template.render(context, request))
+
+    elif request.method == 'POST':
+        try:
+            parent_task = Task.objects.get(pk=request.POST.get("parent_id", 0))
+        except Task.DoesNotExist as e:
+            parent_task = None
+        
+
+        new_task = Task(name=request.POST.get("title", "no title"),
+            description=request.POST.get("description", "no description"),
+            parent=parent_task)
+
+        new_task.save()
+        return HttpResponseRedirect(reverse('tasks:detail', args=(new_task.id,)))
+
