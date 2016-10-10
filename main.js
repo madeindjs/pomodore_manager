f93a0556387512ae2bd5e0f923c0da78edb6d6c6
madeindjs <madeindjs@gmail.com>
2016-10-10 19:13:04 +0200

    make sone tests
    :q

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 var http = require('http');
+var url = require("url");
 
+var port = 12345;
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer();
+console.log('server started on localhost: http://localhost:'+port+'/')
+
+server.on('request', function(req, res) {
+  console.log( url.parse(req.url).pathname );
   res.writeHead(200, {"Content-Type": "text/html"});
   res.end('Salut tout le monde !');
-});
+})
 
-server.listen(12345);
+server.on( 'close', function() {
+  console.log('Bye bye !'); 
+})
+
+server.listen(port);
