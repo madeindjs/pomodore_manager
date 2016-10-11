88e4dce280341821754d74d99cb30935bab07dea
madeindjs <madeindjs@gmail.com>
2016-10-11 14:03:04 +0200

    create render with template system

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var server = require('./server').start(12345);
 
 server.on('tasks#index', function(response){
-	response.write('<h1>task of index</h1>');
+	response.render('views/layout.html', {title: 'task of index'});
 })
 
 server.on('tasks#show', function(response, data){
-	response.write('<h1>task n°'+data.id+'</h1>');
+	response.render('views/layout.html', {title: 'Task #'+data.id});
 })
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require("url");
+var fs = require("fs");
 const EventEmitter = require("events");
 
 
@@ -9,23 +10,48 @@ var App = {
 		var emiter = new EventEmitter() ;
 
 		http.createServer(function(request, response){
-			response.writeHead(200, {"Content-Type": "text/html"});
+
+			
+
+			// create the function to render an Html file
+			response.render = function(file, items){
+
+				//open the file
+				fs.readFile(file, 'utf-8' ,function(errors, file_data){
+					
+					if(errors){throw errors;}
+					// replace data file_data
+					for(var key in items){
+						// an ugly trick to imitate an `String.replaceAll()` method
+						file_data = file_data.split('{{'+key+'}}').join(items[key]);
+					}
+					response.write(file_data);
+					// close 
+					response.end();
+
+				})// close file
+
+			}
 
 
+			// parse url
 			var url_array = request.url.split('/') ;
-
 			var controller = url_array[1] ;
 			var action = url_array.length >= 3 ? url_array[2] : 'index' ;
 			var id = url_array.length >= 4 ? url_array[3] : undefined ;
-
 			var data = { controller: controller, action: action, id: id };
 			console.log(data);
 
+			//emit the signal
 			var signal_name = controller+'#'+action;
-
 			emiter.emit(signal_name, response, data );
 
-			response.end();
+
+
+			console.log(response.render)
+
+			
+
 		}).listen(port);
 
 		return emiter;
