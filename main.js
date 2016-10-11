81fbf06221482cceccd79807097be804b7e98cc6
madeindjs <madeindjs@gmail.com>
2016-10-11 13:04:46 +0200

    build a basic router

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var server = require('./server').start(12345);
 
-server.on('root', function(response){
-	response.write('hello')
+server.on('tasks#index', function(response){
+	response.write('<h1>task of index</h1>');
+})
+
+server.on('tasks#show', function(response, data){
+	response.write('<h1>task n°'+data.id+'</h1>');
 })
--- server.js
+++ server.js
@@ -11,9 +11,19 @@ var App = {
 		http.createServer(function(request, response){
 			response.writeHead(200, {"Content-Type": "text/html"});
 
-			if(request.url === '/'){
-				emiter.emit('root', response)
-			}
+
+			var url_array = request.url.split('/') ;
+
+			var controller = url_array[1] ;
+			var action = url_array.length >= 3 ? url_array[2] : 'index' ;
+			var id = url_array.length >= 4 ? url_array[3] : undefined ;
+
+			var data = { controller: controller, action: action, id: id };
+			console.log(data);
+
+			var signal_name = controller+'#'+action;
+
+			emiter.emit(signal_name, response, data );
 
 			response.end();
 		}).listen(port);
